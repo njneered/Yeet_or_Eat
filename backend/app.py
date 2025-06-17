edf48b1f8616eb3cbec3b05599b56e60a12f32b8
satovar16 <satovarb@gmail.com>
2025-06-17 17:24:28 -0400

    Fixed issue #21: Implementing review model and review sumbission functionality

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 print("app.py loaded")
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session
 from flask_cors import CORS
 from config_settings import Config
-from db_models import User, db
+from db_models import User, db, Review
 
 app = Flask(__name__)
 CORS(app)
@@ -19,6 +19,7 @@ def login():
 
     valid_user = User.query.filter_by(email=email).first()
     if valid_user and valid_user.checkPassword(password):
+        session['user_id'] = valid_user.id
         return jsonify({"message" : "Login successful!"}), 200
     return jsonify({"message" : "Invalid credentials"}), 401
 
@@ -48,6 +49,43 @@ def signup():
 @app.route('/ping', methods=['GET'])
 def ping():
     return jsonify({"message": "pong"}), 200
+
+@app.route('/review', methods=['POST'])
+def review():
+    #Check user is logged
+    if 'user_id' not in session:
+        return jsonify({"message": "You need to log in to post a review! Thanks!"}), 401
+
+    # Get user_id
+    user_id = session['user_id']
+
+    # Get the review parameters
+    data = request.get_json()
+    restaurant = data.get('restaurant')
+    dish = data.get('dish')
+    rating = data.get('rating')
+    comment = data.get('comment')
+
+    if not restaurant or not dish or not rating:
+        return jsonify({"message" : "All fields are required!"}), 400
+
+    if rating < 1 or rating > 5:
+        return jsonify({"message" : "Rating must be between 1 and 5!"})
+
+    # Create the new review
+    new_review = Review(
+        user_id=user_id,
+        restaurant=restaurant,
+        dish=dish,
+        rating=rating,
+        comment=comment
+    )
+
+    # Add the review to the database
+    db.session.add(new_review)
+    db.session.commit()
+
+    return jsonify({"message": "Review added successfully! Thank you!"}), 201
 
 if __name__ == "__main__":
     app.run(debug=True, port =5050)
