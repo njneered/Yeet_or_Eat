e2a069937720e3b7181aadebf91137938e3f1662
Dylan <160562249+ok-Mook@users.noreply.github.com>
2025-06-25 17:42:10 -0400

    Did i do something?

--- backend/run_backend.py
+++ backend/run_backend.py
@@ -1,6 +1,14 @@
-from app import app
+from app import app, db
+from flask_migrate import Migrate
+
+
+migrate = Migrate(app, db)
 
 print("🚀 Backend booting...")
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"app": app, "db": db}
 
 if __name__ == "__main__":
     app.run(debug=True, port=5050)
