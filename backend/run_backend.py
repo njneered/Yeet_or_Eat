02a056aed87d95476184a70d4f8a67d9dca55207
Dylan <160562249+ok-Mook@users.noreply.github.com>
2025-06-25 17:44:47 -0400

    Removed pip dependencies and switched to Poetry

--- backend/run_backend.py
+++ backend/run_backend.py
@@ -1,14 +1,6 @@
-from app import app, db
-from flask_migrate import Migrate
-
-
-migrate = Migrate(app, db)
+from app import app
 
 print("🚀 Backend booting...")
-
-@app.shell_context_processor
-def make_shell_context():
-    return {"app": app, "db": db}
 
 if __name__ == "__main__":
     app.run(debug=True, port=5050)
